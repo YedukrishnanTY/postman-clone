82717d79b4b82d7fcdfcc9a9c18d65a88de41d20
Yedu Krishnan T Y <yedu345@gmail.com>
2024-04-21 18:57:44 +0530

    unwanted

--- client-ui/pages/_app.js
+++ client-ui/pages/_app.js
@@ -3,7 +3,6 @@ import React from "react";
 import Layout from "@/layout";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 
 export default function App({ Component, pageProps }) {
   const [mounted, setMounted] = React.useState(false);
@@ -18,7 +17,7 @@ export default function App({ Component, pageProps }) {
       <NextThemesProvider
         attribute="class"
         defaultTheme="dark"
-        themes={["light", "dark", "modern"]}
+        themes={["light", "dark"]}
       >
         <Layout pageProps={pageProps}>
           <Component {...pageProps} />
--- client-ui/pages/index.js
+++ client-ui/pages/index.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import Head from "next/head";
 import HomePage from "@/views/HomePage";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export async function getServerSideProps(context) {
   return {
