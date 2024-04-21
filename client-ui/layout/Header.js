cdc5842311dfc051a1ddacc1640e6a638d6cc53f
Yedu Krishnan T Y <yedu345@gmail.com>
2024-04-21 09:52:01 +0530

    NEXT UI & NEXT THEMES

--- client-ui/layout/Header.js
+++ client-ui/layout/Header.js
@@ -1,5 +1,35 @@
 import React from "react";
-
+import { Button } from "@nextui-org/react";
+import { useTheme } from "next-themes";
 export default function UI({}) {
-  return <h1>Header</h1>;
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
+  return (
+    <div>
+      <div>
+        <Button
+          color="primary"
+          variant="flat"
+          radius="full"
+          onClick={() => setTheme("light")}
+        >
+          Light
+        </Button>
+        <Button
+          color="primary"
+          variant="flat"
+          radius="full"
+          onClick={() => setTheme("dark")}
+        >
+          Dark
+        </Button>
+      </div>
+    </div>
+  );
 }
--- client-ui/pages/_app.js
+++ client-ui/pages/_app.js
@@ -1,10 +1,29 @@
 import "@/styles/globals.css";
+import React from "react";
 import Layout from "@/layout";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 
 export default function App({ Component, pageProps }) {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return null;
+  }
   return (
-    <Layout pageProps={pageProps}>
-      <Component {...pageProps} />
-    </Layout>
+    <NextUIProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="dark"
+        themes={["light", "dark", "modern"]}
+      >
+        <Layout pageProps={pageProps}>
+          <Component {...pageProps} />
+        </Layout>
+      </NextThemesProvider>
+    </NextUIProvider>
   );
 }
